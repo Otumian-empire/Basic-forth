146fe56b4860fb1df0454983020b4eb309ed41b1
otumian <popecan1000@gmail.com>
2020-04-11 18:43:20 +0000

    problem 7 from chapter 2

---
+++ chapter2/prob7.fs
@@ -0,0 +1,9 @@
+( You're the inventory programmer at Maria's Egg Ranch.
+Define a word called EGG.CARTONS which expects on the stack
+the total number of eggs laid by the chickens today and
+prints out the number of cartons that can be filled with a
+dozen each, as well as the number of left-over eggs. )
+
+: EGG.CARTONS ( numberOfEggs -- numberOfCartons remainderOfEgss )
+12 /MOD SWAP ." There is (are) " . ." carton(s) of egg(s) and " . ." other egg(s)." ;
+
